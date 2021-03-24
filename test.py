565eab71ea2995974873c42aabcc6b0a19f4278d
Konstantin Ivanov <69236482+TredNR@users.noreply.github.com>
2021-03-24 21:31:53 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 from flask import Flask
-from flask_cors import CORS, cross_origin
-test = Flask(__name__)
-cors = CORS(test)
-test.config['CORS_HEADERS'] = 'Content-Type'
 
-@test.route("/")
-@cross_origin()
-def helloWorld():
-  return "Hello, cross-origin-world!"
+test = Flask(__name__)
+
+@test.route('/')
+def hello():
+    return "Привет, мир!"
+
+
+@test.route('/<name>')
+def hello_name(name):
+    return f"Привет, {name}!"
+
+
+if __name__ == '__main__':
+    test.run()
