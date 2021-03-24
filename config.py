5354aafae197650d552bddebb02c0256680573aa
Konstantin Ivanov <69236482+TredNR@users.noreply.github.com>
2021-03-24 20:22:51 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'b5fa374447f10b'
 app.config['MYSQL_DATABASE_PASSWORD'] = '7e50404a'
-app.config['MYSQL_DATABASE_DB'] = 'lab4and5'
+app.config['MYSQL_DATABASE_DB'] = 'heroku_8da7b2bec58bce2'
 app.config['MYSQL_DATABASE_HOST'] = 'eu-cdbr-west-03.cleardb.net'
 mysql.init_app(app)
